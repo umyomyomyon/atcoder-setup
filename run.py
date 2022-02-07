87807391097a0447cbf1922f205100e81946751c
umyomyomyon <minecraft.sentry.yym@gmail.com>
2022-02-07 15:21:29 +0900

    ファイル名変更

--- run.py
+++ run.py
@@ -2,16 +2,15 @@ import os
 import sys
 import subprocess
 
-input_file_list = [input_file for input_file in os.listdir(path='./inputs') if '.txt' in input_file]
+input_flie_list = sorted([input_file for input_file in os.listdir(path='./inputs') if '.txt' in input_file])
 
-
-def make_commands():
+def make_commands(input_file_list):
     return [
-        f'python solve.py < ./inputs/{file_name} > ./answers/answer_{idx + 1}.txt'
+        f'python3 solve.py < ./inputs/{file_name} > ./answers/{idx + 1}.txt'
         for idx, file_name in enumerate(input_file_list)
     ]
 
 
-commands = make_commands()
+commands = make_commands(input_flie_list)
 for command in commands:
     subprocess.run(command, shell=True, text=True, stdout=sys.stdout)
